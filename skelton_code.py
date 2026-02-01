647cc992fcc946bace74d510be918588e8fa1306
Ely Cheikh Abass <elycheikh2002@hotmail.com>
2026-02-01 01:56:53 +0100

    adding cross validation

--- skelton_code.py
+++ skelton_code.py
@@ -5,6 +5,7 @@ geojsons, to train a trivial model, and write data to the submission file.
 import geopandas as gpd
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.base import clone
 import numpy as np
 
 
@@ -28,14 +29,42 @@ train_y = train_df['change_type'].apply(lambda x: change_type_map[x])
 test_x = np.asarray(test_df[['geometry']].area)
 test_x = test_x.reshape(-1, 1)
 
-print (train_x.shape, train_y.shape, test_x.shape)
+print(train_x.shape, train_y.shape, test_x.shape)
 
+
+def cross_validation(k, model):
+    n = len(train_x)
+    fold_size = n // k
+    idx = np.arange(n)
+    acc_sum = 0
+    for f in range(k):
+        start = f * fold_size
+        end = (f + 1) * fold_size if f < k - 1 else n
+
+        mask_train = (idx < start) | (idx >= end)
+
+        mini_train_x = train_x[mask_train, :]
+        mini_train_y = train_y[mask_train]
+
+        val_x = train_x[start:end, :]
+        val_y = train_y[start:end]
+
+        m = clone(model)
+        m.fit(mini_train_x, mini_train_y)
+        y_pred = m.predict(val_x)
+        accuracy = accuracy_score(val_y, y_pred)
+        acc_sum += accuracy
+
+    print(f"Cross validation: {k} folds, mean accuracy = {acc_sum/k}")
 
 ## Train a simple OnveVsRestClassifier using featurized data
 neigh = KNeighborsClassifier(n_neighbors=3)
+
+cross_validation(15, neigh)
 neigh.fit(train_x, train_y)
 pred_y = neigh.predict(test_x)
-print (pred_y.shape)
+
+print(pred_y.shape)
 
 ## Save results to submission file
 pred_df = pd.DataFrame(pred_y, columns=['change_type'])
